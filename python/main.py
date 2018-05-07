4495493e0fbe02531eadf757beacaeec4066a75c
Kenny Lam <hk_lam@live.com>
2018-05-07 23:21:53 +0200

    Fix Breathing

--- python/main.py
+++ python/main.py
@@ -25,8 +25,8 @@ continue_reading = True
 def end_read(signal, frame):
     global continue_reading
     print "Ctrl+C captured, ending read."
-    continue_reading = False
     resetLeds(ring, BLACK)
+    continue_reading = False
     pass
 
 
@@ -57,18 +57,20 @@ def stroboscopeEffect(strip, color, wait_ms=50, iterations=10):
                 strip.setPixelColor(i + q, 0)
 
 
-def breathing(strip, color, wait_ms=50, maxbrightness=255):
+def breathing(strip, color, wait_ms=30, maxbrightness=255):
     for j in range(0, 256):
         strip.setBrightness(j)
         for j in range(strip.numPixels()):
             strip.setPixelColor(j, color)
             strip.show()
+            time.sleep(wait_ms / 1000)
 
     for j in range(0, 256):
         strip.setBrightness(maxbrightness - j)
         for j in range(strip.numPixels()):
             strip.setPixelColor(j, color)
             strip.show()
+            time.sleep(wait_ms / 1000)
 
 def resetLeds(ring, color, wait_ms=10):
     for i in range(ring.numPixels()):
